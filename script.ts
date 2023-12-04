eca56dacb50b1f64868b073a78f2b17c143ae9fc
Leonardo Griesang <griesang.leonardo@outlook.com>
2023-12-04 15:26:13 -0300

    melhoria no fetch e lógica para o refrash

--- script.ts
+++ script.ts
@@ -1,14 +1,19 @@
-async function fetchApi() {
+async function fetchTeste() {
     const response = await fetch('https://rickandmortyapi.com/api/character/?page=1');
     const json = await response.json();
-    mostrarPersonagens(json)
+    console.log(json)
 }
-fetchApi();
+fetchTeste();
 
-interface personagem {
-    info: {};
-    results: resultado[];
+async function fetchApi(data1: string, data2: string, data3: string, data4: string, data5: string, data6: string) {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${data1}, ${data2}, ${data3}, ${data4}, ${data5}, ${data6}`);
+    const json = await response.json();
+    mostrarPersonagens(json)
+    console.log(json)
 }
+fetchApi('1', '2', '3', '4', '5', '6');
+
+
 interface resultado {
     id: number;
     image: string;
@@ -23,92 +28,104 @@ interface resultado {
     };
 }
 
-function mostrarPersonagens(data: personagem) {
+function mostrarPersonagens(data: resultado[]) {
     const img1 = document.getElementById('img-1');
     const conteudo1 = document.getElementById('conteudo-1');
     if (img1 instanceof HTMLImageElement && conteudo1 instanceof HTMLElement) {
-        img1.setAttribute('src', data.results[0].image)
+        img1.setAttribute('src', data[0].image)
         conteudo1.innerHTML = `
-            <h1>${data.results[0].name}</h1>
-            <h2>${data.results[0].status} - <span>${data.results[0].species}</span></h2>
+            <h1>${data[0].name}</h1>
+            <h2>${data[0].status} - <span>${data[0].species}</span></h2>
             <h3>Last known location:</h3>
-            <p>${data.results[0].location.name}</p>
+            <p>${data[0].location.name}</p>
             <h3>origin:</h3>
-            <p>${data.results[0].origin.name}</p>
+            <p>${data[0].origin.name}</p>
         `
     }
 
     const img2 = document.getElementById('img-2');
     const conteudo2 = document.getElementById('conteudo-2');
     if (img2 instanceof HTMLImageElement && conteudo2 instanceof HTMLElement) {
-        img2.src = `${data.results[1].image}`
+        img2.src = `${data[1].image}`
         conteudo2.innerHTML = `
-        <h1>${data.results[1].name}</h1>
-        <h2>${data.results[1].status} - <span>${data.results[1].species}</span></h2>
+        <h1>${data[1].name}</h1>
+        <h2>${data[1].status} - <span>${data[1].species}</span></h2>
         <h3>Last known location:</h3>
-        <p>${data.results[1].location.name}</p>
+        <p>${data[1].location.name}</p>
         <h3>origin:</h3>
-        <p>${data.results[1].origin.name}</p>
+        <p>${data[1].origin.name}</p>
         `
     }
 
     const img3 = document.getElementById('img-3');
     const conteudo3 = document.getElementById('conteudo-3');
     if (img3 instanceof HTMLImageElement && conteudo3 instanceof HTMLElement) {
-        img3.src = `${data.results[3].image}`
+        img3.src = `${data[3].image}`
         conteudo3.innerHTML = `
-        <h1>${data.results[3].name}</h1>
-        <h2>${data.results[3].status} - <span>${data.results[3].species}</span></h2>
+        <h1>${data[3].name}</h1>
+        <h2>${data[3].status} - <span>${data[3].species}</span></h2>
         <h3>Last known location:</h3>
-        <p>${data.results[3].location.name}</p>
+        <p>${data[3].location.name}</p>
         <h3>origin:</h3>
-        <p>${data.results[3].origin.name}</p>
+        <p>${data[3].origin.name}</p>
         `
     }
 
     const img4 = document.getElementById('img-4');
     const conteudo4 = document.getElementById('conteudo-4');
     if (img4 instanceof HTMLImageElement && conteudo4 instanceof HTMLElement) {
-        img4.src = `${data.results[9].image}`
+        img4.src = `${data[2].image}`
         conteudo4.innerHTML = `
-        <h1>${data.results[9].name}</h1>
-        <h2>${data.results[9].status} - <span>${data.results[9].species}</span></h2>
+        <h1>${data[2].name}</h1>
+        <h2>${data[2].status} - <span>${data[2].species}</span></h2>
         <h3>Last known location:</h3>
-        <p>${data.results[9].location.name}</p>
+        <p>${data[2].location.name}</p>
         <h3>origin:</h3>
-        <p>${data.results[9].origin.name}</p>
+        <p>${data[2].origin.name}</p>
         `
     }
 
     const img5 = document.getElementById('img-5');
     const conteudo5 = document.getElementById('conteudo-5');
     if (img5 instanceof HTMLImageElement && conteudo5 instanceof HTMLElement) {
-        img5.src = `${data.results[6].image}`
+        img5.src = `${data[4].image}`
         conteudo5.innerHTML = `
-        <h1>${data.results[6].name}</h1>
-        <h2>${data.results[6].status} - <span>${data.results[6].species}</span></h2>
+        <h1>${data[4].name}</h1>
+        <h2>${data[4].status} - <span>${data[4].species}</span></h2>
         <h3>Last known location:</h3>
-        <p>${data.results[6].location.name}</p>
+        <p>${data[4].location.name}</p>
         <h3>origin:</h3>
-        <p>${data.results[6].origin.name}</p>
+        <p>${data[4].origin.name}</p>
         `
     }
 
     const img6 = document.getElementById('img-6');
     const conteudo6 = document.getElementById('conteudo-6');
     if (img6 instanceof HTMLImageElement && conteudo6 instanceof HTMLElement) {
-        img6.src = `${data.results[14].image}`
+        img6.src = `${data[5].image}`
         conteudo6.innerHTML = `
-        <h1>${data.results[14].name}</h1>
-        <h2>${data.results[14].status} - <span>${data.results[14].species}</span></h2>
+        <h1>${data[5].name}</h1>
+        <h2>${data[5].status} - <span>${data[5].species}</span></h2>
         <h3>Last known location:</h3>
-        <p>${data.results[14].location.name}</p>
+        <p>${data[5].location.name}</p>
         <h3>origin:</h3>
-        <p>${data.results[14].origin.name}</p>
+        <p>${data[5].origin.name}</p>
         `
     }
+}
 
-    data.results.forEach((data) => {
-        console.log(data);
-    })
+const btn = document.getElementById('btn');
+
+btn?.addEventListener('click', refresh);
+
+function refresh(event: MouseEvent) {
+    event.preventDefault();
+    let num1 = Math.floor(Math.random() * 826).toLocaleString();
+    let num2 = Math.floor(Math.random() * 826).toLocaleString();
+    let num3 = Math.floor(Math.random() * 826).toLocaleString();
+    let num4 = Math.floor(Math.random() * 826).toLocaleString();
+    let num5 = Math.floor(Math.random() * 826).toLocaleString();
+    let num6 = Math.floor(Math.random() * 826).toLocaleString();
+
+    fetchApi(num1, num2, num3, num4, num5, num6);
 }
